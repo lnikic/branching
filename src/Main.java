281f857aecabd74346ccc71e6bd5708af8a009a3
nikic <nikic.luka2001@gmail.com>
2025-10-29 13:58:46 +0100

    2. quiz for bottles

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,27 @@ public class Main {
             a = a + 1;
         }
         if( a == 3){
-            System.out.print("Do");
+            System.out.print("Do\n");
+        }
+
+        int bottlesNum = 10;
+        String word = "bottles";
+
+        while (bottlesNum > 0){
+
+            if(bottlesNum == 1){
+                word = "bottle";
+            }
+            System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+            System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+            System.out.println("And if one green bottle should accidentally fall,");
+            bottlesNum = bottlesNum - 1;
+
+            if (bottlesNum > 0) {
+                System.out.println("There'll be " + bottlesNum +" green " + word + ", hanging on the wall");
+            } else {
+                System.out.println("There'll be no green bottles, hanging on the wall");
+            }
         }
     }
 }
