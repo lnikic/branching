24ce8fbe8e6e771861f0fc415463d016abd0b1fe
nikic <nikic.luka2001@gmail.com>
2025-10-29 13:48:04 +0100

    branching and quiz solved

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x is not 3");
+        }
+        System.out.println("This run no matter what");
 
-        System.out.println("Hello, World!");
+
+        int a = 1;
+        while (a < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            a = a + 1;
+        }
+        if( a == 3){
+            System.out.print("Do");
+        }
     }
 }
